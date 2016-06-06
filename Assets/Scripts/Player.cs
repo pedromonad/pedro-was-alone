44a549d039aa2f5e354b6da01deae5d1ad16d0a5
Pedro Augusto de Barros <pedro.barros@s2it.com.br>
2016-06-06 08:48:26 -0300

    Project - Finished

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour 
+{
+	
+	public float m_JumpForce;
+
+	[Range(0.0f, 10.0f)]
+	public float m_Speed;
+	public bool m_IsGrounded;
+	public bool Enabled { get; set; }
+
+	//[Serialize]
+	//private float m_JumpForce;
+	private Rigidbody2D m_Rigidbody;
+	private Transform m_Transform;
+	private float m_Horizontal;
+	private bool m_Jump;
+	private Vector2 m_Movement;
+	private Vector3 m_StartPosition;
+
+	private void Start()
+	{
+		m_StartPosition = m_Transform.position;
+	}
+		
+	private void Awake()
+	{
+		m_Rigidbody = GetComponent<Rigidbody2D> ();
+		m_Transform = GetComponent<Transform> ();
+
+	}
+
+	private void Update()
+	{
+		if (!Enabled) return;
+
+		m_Horizontal = Input.GetAxis ("Horizontal");
+		m_Jump = Input.GetButtonDown ("Jump");
+
+		if (m_Jump && m_IsGrounded)
+			m_Rigidbody.AddForce (Vector2.up * m_JumpForce);
+
+		m_Movement = Vector2.right * m_Horizontal * m_Speed * Time.deltaTime;
+		m_Transform.Translate (m_Movement);
+	}
+
+	private void OnTriggerEnter2D(Collider2D hit){
+		if (hit.CompareTag ("DeadZone"))
+			m_Transform.position = m_StartPosition;
+	}
+
+
+	private void OnTriggerStay2D(Collider2D hit)
+	{
+		m_IsGrounded = true;
+	}
+
+	private void OnTriggerExit2D(Collider2D hit)
+	{
+		m_IsGrounded = false;
+	}
+
+
+
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	private Player[] m_Players;
+	private int m_CurrentPlayer = 0;
+	private CameraFollow m_CameraFollow;
+
+	private void Start()
+	{
+		m_CameraFollow = FindObjectOfType<CameraFollow> ();
+		m_Players = FindObjectsOfType<Player> ();
+		m_Players [m_CurrentPlayer].Enabled = true;
+		m_CameraFollow.ChangeTarget (m_Players [m_CurrentPlayer].transform);
+
+	}
+		
+	private void Update(){
+		if (Input.GetButtonDown ("Fire2"))
+		{
+			ChangePlayer ();
+			m_CameraFollow.ChangeTarget (m_Players [m_CurrentPlayer].transform);
+		}
+	}
+
+	private void ChangePlayer()
+	{
+		m_Players[m_CurrentPlayer].Enabled = false;
+		m_CurrentPlayer = (++m_CurrentPlayer) % m_Players.Length;
+		m_Players [m_CurrentPlayer].Enabled = true;
+
+	}
+
+
+	private void Save()
+	{
+		for (int i = 0; i < m_Players.Length; i++) {
+			if (PlayerPrefs.HasKey ("Player" + i))
+				m_Players [i].transform.position = JsonUtility.FromJson<Vector3> (PlayerPrefs.GetString ("Player" + i));
+
+			m_CurrentPlayer = PlayerPrefs.GetInt ("CurrentPlayer");
+			m_Players [0].Enabled = false;
+			m_Players [m_CurrentPlayer].Enabled = true;
+			
+		}
+
+	}
+
+
+
+
+}
